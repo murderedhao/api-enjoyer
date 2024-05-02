85bef14b5541257b4ecd2705ae7338c7f307d42a
murderedhao <f1dg1@mail.ru>
2024-05-02 18:03:22 +0300

    Styles for MyCartPage

--- src/components/pages/MyCartPage/MyCartPage.jsx
+++ src/components/pages/MyCartPage/MyCartPage.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import styles from './MyCartPage.module.scss'
 
 const MyCartPage = () => {
 	const cartItems = useSelector(state => state.item.transactions.items)
 	return (
-		<div>
-			<h2>Items in Cart</h2>
-			<div>
-				{cartItems.map((item, index) => (
-					<div key={index}>
-						<img src={item.images} alt={`Item ${index}`} />
-						<p>{item.title}</p>
-					</div>
-				))}
-			</div>
+		<div className={styles.container}>
+			<h1 className={styles.container__head}>Items in Cart</h1>
+			{cartItems.map((item, index) => (
+				<div className={styles.container__item} key={index}>
+					<img
+						className={styles.container__img}
+						src={item.images}
+						alt={`Item ${index}`}
+					/>
+					<p className={styles.container__title}>{item.title}</p>
+				</div>
+			))}
 		</div>
 	)
 }
